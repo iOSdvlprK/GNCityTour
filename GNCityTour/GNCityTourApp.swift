1135209fb23cadfd7e7090a2003e567a5fedaa68
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-24 20:51:10 +0900

    add Keyword model and rename ContentView to PlacesView
    - introduced a new Keyword enum model to represent place categories with display and API names
    - renamed ContentView.swift to PlacesView.swift and updated references in GNCityTourApp to use PlacesView as the main entry point

--- GNCityTour/GNCityTourApp.swift
+++ GNCityTour/GNCityTourApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GNCityTourApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlacesView()
         }
     }
 }
--- GNCityTour/PlacesView.swift
+++ GNCityTour/PlacesView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PlacesView.swift
 //  GNCityTour
 //
 //  Created by joe on 1/24/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PlacesView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    PlacesView()
 }
