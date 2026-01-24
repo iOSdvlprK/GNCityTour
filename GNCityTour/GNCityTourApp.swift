8b86bed9f4a0fe56bebbc367360b47d5c407dc34
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-24 20:18:21 +0900

    Initial Commit

---
+++ GNCityTour/GNCityTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNCityTourApp.swift
+//  GNCityTour
+//
+//  Created by joe on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct GNCityTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
