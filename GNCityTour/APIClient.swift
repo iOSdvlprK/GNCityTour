1b470b8ae7f09a18a32f9e483cb7b6ff165d9d7d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-26 22:19:53 +0900

    add URL creation helper to APIClient
    - introduced a private createURL method in APIClient to construct Google Places API URLs using latitude, longitude, and keyword parameters
    - this refactoring improves code organization and reusability for building API requests

--- GNCityTour/APIClient.swift
+++ GNCityTour/APIClient.swift
@@ -6,8 +6,21 @@
 //
 
 import Foundation
+import CoreLocation
 
 class APIClient {
     private let baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     private let googlePlacesKey = apiKey
+    
+    private func createURL(latitude: Double, longitude: Double, keyword: String) -> URL? {
+        var urlComponents = URLComponents(string: baseURL)
+        var queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "location", value: "\(latitude),\(longitude)"),
+            URLQueryItem(name: "rankby", value: "distance"),
+            URLQueryItem(name: "keyword", value: keyword),
+            URLQueryItem(name: "key", value: googlePlacesKey)
+        ]
+        urlComponents?.queryItems = queryItems
+        return urlComponents?.url
+    }
 }
