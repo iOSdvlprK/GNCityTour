efc116b96597e8e5b7f454488f157efb40d241f3
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-29 22:49:53 +0900

    refactor model properties to camelCase and add CodingKeys
    - updated PlaceDetailResponseModel and PhotoInfo to use camelCase property names and added CodingKeys enums for correct JSON decoding

--- GNCityTour/Models/PlacesResponseModel.swift
+++ GNCityTour/Models/PlacesResponseModel.swift
@@ -12,13 +12,22 @@ struct PlacesResponseModel: Decodable {
 }
 
 struct PlaceDetailResponseModel: Decodable {
-    let place_id: String
+    let placeId: String
     let name: String
     let rating: Double
     let vicinity: String
     let photos: [PhotoInfo]?
+    
+    enum CodingKeys: String, CodingKey {
+        case placeId = "place_id"
+        case name, rating, vicinity, photos
+    }
 }
 
 struct PhotoInfo: Decodable {
-    let photo_reference: String
+    let photoReference: String
+    
+    enum CodingKeys: String, CodingKey {
+        case photoReference = "photo_reference"
+    }
 }
